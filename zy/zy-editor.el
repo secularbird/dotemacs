6eff3d8a85508012549a524881a7bcf2731497bd
zhuangyao <secularbird.eagle@gmail.com>
2013-01-20 22:23:57 +0800

    add for mac os copy and paste

--- zy/zy-editor.el
+++ zy/zy-editor.el
@@ -87,6 +87,21 @@
         interprogram-paste-function
         'x-cut-buffer-or-selection-value))
 
+;;osx should using pbpaste and pbcopy to share clipboard
+(defun copy-from-osx ()
+	(shell-command-to-string "pbpaste"))
+
+(defun paste-to-osx (text &optional push)
+	(let ((process-connection-type nil))
+	(let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+	(process-send-string proc text)
+	(process-send-eof proc))))
+
+
+(when (string= system-type "darwin")
+	(setq interprogram-cut-function 'paste-to-osx)
+	(setq interprogram-paste-function 'copy-from-osx))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (provide 'zy-editor)
