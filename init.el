bdcea0ef3fecd87cc393b71721e6d072da7d91aa
zhuangyao <secularbird.eagle@gmail.com>
2013-05-26 21:51:24 +0800

    change hbin to zy

--- init.el
+++ init.el
@@ -1,30 +1,3 @@
-;; init.el --- Emacs for hbin starts from here, let's go!
-;;
-;; Copyright (C) 2012 Huang Bin
-;;
-;; Author: Huang Bin <embrace.hbin@gmail.com>
-;; Version: 1.0.0
-
-;; This file is not part of GNU Emacs.
-
-;;; License:
-
-;; This program is free software: you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-;;; Code:
-
-;; Who am I?
 (setq user-full-name "Zhuang Yao")
 (setq user-mail-address "secularbird.eagle@gmail.com")
 (setq global-mode-string (message "  %s  " user-full-name))
@@ -71,8 +44,8 @@
 ;; load configurations.
 (require 'zy-ui)                      ; eye candy
 (require 'zy-editor)                  ; enhance the editor
-(require 'hbin-defuns)                  ; useful tricks
-(require 'hbin-kbds)                    ; key bindings
+(require 'zy-defuns)                  ; useful tricks
+(require 'zy-kbds)                    ; key bindings
 (require 'zy-misc)                    ; miscellaneous
 (require 'zy-prog)                    ; programming
 
--- zy/zy-defuns.el
+++ zy/zy-defuns.el
@@ -1,29 +1,3 @@
-;; hbin-defuns.el --- Useful tricks, part of them stolen from others, thanks you all!
-;;
-;; Copyright (C) 2012 Huang Bin
-;;
-;; Author: Huang Bin <embrace.hbin@gmail.com>
-;; Version: 1.0.0
-
-;; This file is not part of GNU Emacs.
-
-;;; License:
-
-;; This program is free software: you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-;;; Code:
-
 ;; Jump between parent
 (defun match-paren (arg)
   "Go to the matching parenthesis if on a paren."
@@ -263,6 +237,6 @@ indent yanked text (with prefix arg don't indent)."
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(provide 'hbin-defuns)
+(provide 'zy-defuns)
 
-;;;;; hbin-defuns.el END
+;;;;; zy-defuns.el END
