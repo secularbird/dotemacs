458982c6614e4745d98543752fb8ed268dff5b1e
zhuangyao <secularbird.eagle@gmail.com>
2013-11-02 21:42:43 +0800

    add python smart indent

--- zy/prog-python.el
+++ zy/prog-python.el
@@ -1,31 +1,8 @@
-;; prog-python.el --- Enhance Python programming
-;;
-;; Copyright (C) 2012 Huang Bin
-;;
-;; Author: Huang Bin <embrace.hbin@gmail.com>
-;; Version: 1.0.0
-
-;; This file is not part of GNU Emacs.
-
-;;; License:
-
-;; This program is free software: you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-;;; Code:
-
-;;;###autoload
 (progn
+	(add-hook 'python-mode-hook guess-style-guess-tabs-mode)
+   	(add-hook 'python-mode-hook (lambda ()
+    	                              (when indent-tabs-mode
+        	                            (guess-style-guess-tab-width)))
   )
 
 (provide 'prog-python)
